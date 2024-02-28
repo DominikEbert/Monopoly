724bc8cd81dc2087ba012aeb6edd5134d483a4c2
David <amadeus6319@web.de>
2024-02-28 15:43:15 +0100

    Schillerstraße und Turmstraße hinzugefügt

--- src/de/niederseeon/monopoly/MonopolyMain.java
+++ src/de/niederseeon/monopoly/MonopolyMain.java
@@ -26,12 +26,56 @@ public class MonopolyMain {
 		}
 		Immobilie strasse = new Immobilie();
 		
-		Immobilie badstrasse = new Immobilie();
-		badstrasse.setName("badstrasse");
+		Immobilie turmstrasse = new Immobilie();
+		
+		erstelleTurmstrasse(turmstrasse);
+			
+		
 		
 		
 		System.out.println(badstrasse.getName());
 		
 		
+	}
+
+	private static void erstelleTurmstrasse(Immobilie turmstrasse) {
+		// Turmstraße Daten *******************************************
+			turmstrasse.setName("Turmstraße");
+		
+			turmstrasse.setKaufpreis(1200);
+			turmstrasse.setMietpreis(80);
+			turmstrasse.setHypothekenwert(600);
+			
+			turmstrasse.setMietpreisHaus1(400);
+			turmstrasse.setMietpreisHaus2(1200);
+			turmstrasse.setMietpreisHaus3(3600);
+			turmstrasse.setMietpreisHaus4(6400);
+			turmstrasse.setMietpreisHotel(9000);
+			
+			turmstrasse.setPreisHaus1(1000);
+			turmstrasse.setPreisHaus2(1000);
+			turmstrasse.setPreisHaus3(1000);
+			turmstrasse.setPreisHaus4(1000);
+			turmstrasse.setPreisHotel(1000);
+	}
+	private static void erstelleSchillerstrasse(Immobilie schillerstrasse) {
+		// Schillerstraße Daten *******************************************
+			schillerstrasse.setName("Schillerstraße");
+		
+			schillerstrasse.setKaufpreis(5200);
+			schillerstrasse.setMietpreis(480);
+			schillerstrasse.setHypothekenwert(2600);
+			
+			schillerstrasse.setMietpreisHaus1(2200);
+			schillerstrasse.setMietpreisHaus2(6600);
+			schillerstrasse.setMietpreisHaus3(16000);
+			schillerstrasse.setMietpreisHaus4(19500);
+			schillerstrasse.setMietpreisHotel(23000);
+			
+			schillerstrasse.setPreisHaus1(3000);
+			schillerstrasse.setPreisHaus2(3000);
+			schillerstrasse.setPreisHaus3(3000);
+			schillerstrasse.setPreisHaus4(3000);
+			schillerstrasse.setPreisHotel(3000);
 	}
 }
